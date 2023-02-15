d55c243135d6b9a8bfa5d97a518b7779d6f06eda
LEE <tjddbs1412@naver.com>
2023-02-15 14:25:25 +0900

    fastlms 연습 설정 초기화

---
+++ src/main/java/com/example/fastlmsexample/FastlmsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastlmsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastlmsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastlmsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastlmsexample/FastlmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastlmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastlmsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
